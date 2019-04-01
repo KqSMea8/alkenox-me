df09cc15c78c66b3c786ffcd2ae10e438f8f1eba
nox <alk3nox@gmail.com>
2019-04-01 14:56:04 -0400

    empty

--- root/public/web/app/components/Success.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const Success: React.SFC<{}> = () => {
-  return <span>We'll Be In Touch!!</span>;
-};
-
-export default Success;
--- root/src/start.ts
+++
@@ -1,9 +0,0 @@
-import NoxServer from "./NoxServer";
-
-// Start the server or run tests
-if (process.argv[2] !== "test") {
-  let server = new NoxServer();
-  server.start();
-} else {
-  null;
-}
